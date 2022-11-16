103c215548e0692891c82b85f5100e32447f7198
Mahmoud Abu-Attiya <mahmoud.abuattiya106@gmail.com>
2022-11-17 01:34:06 +0200

    insulator links

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//    content: ["./src/**/*.{html,js}"],
+//    theme: {
+//       extend: {},
+//       container: {
+//          center: true,
+//          padding: '2rem',
+//       },
+//    },
+//    plugins: [],
+// }
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: ["./src/**/*.{html,js}"],
+   theme: {
+      extend: {
+         container: {
+            center: true,
+            padding: '2rem',
+         },
+      },
+   },
+   plugins: [],
+}
